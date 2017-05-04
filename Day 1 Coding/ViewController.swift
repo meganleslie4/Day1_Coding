c950dbaf424df0842410a98ac06fbbe14bce8fbf
Megan Leslie <meganleslie4@gmail.com>
2017-05-04 14:45:13 -0600

    My first commit

--- Day 1 Coding/ViewController.swift
+++ Day 1 Coding/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+
+    var tapCount = 0
+    
+    @IBAction func theButton(_ sender: Any) {
+        theLabel.text = "Hello There! 😘"
+        self.view.backgroundColor = UIColor.green
+        //Change background colour to green
+        
+        tapCount = tapCount + 1
+        print(tapCount)
+        
+        if tapCount >= 5 {
+            theLabel.text = "You have pressed the button too many times!"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
